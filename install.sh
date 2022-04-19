dd7805e686507da78c1fffaef96f3c7f4c4c8383
jxjeff <chinamuran404@outlook.com>
2022-04-19 14:05:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -308,7 +308,7 @@ web_camouflage() {
     rm -rf /home/wwwroot
     mkdir -p /home/wwwroot
     cd /home/wwwroot || exit
-    git clone https://git.wej.cc/https://github.com/jxjeff/3DCEList.git
+    git clone /https://github.com/wulabing/3DCEList.git
     judge "web 站点伪装"
 }
 v2ray_install() {
